671550d266262912a64649be53c4fb293e999a32
2022-DEV3-46 <2022dev3046@gmail.com>
2022-07-09 18:07:02 +0530

    initial commit with base project

---
+++ src/components/TicTacToe.js
@@ -0,0 +1,14 @@
+import '../app.css';
+import { Constants } from '../constants/Constants';
+
+function TicTacToe() {
+  return (
+    <div className='App'>
+      <header data-testid='header' className='App-header'>
+        {Constants.HEADER}
+      </header>
+    </div>
+  );
+}
+
+export default TicTacToe;
