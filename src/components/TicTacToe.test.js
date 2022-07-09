33459a98eb8d7ee4fd615cccce924c4e47a3e935
2022-DEV3-46 <2022dev3046@gmail.com>
2022-07-09 19:36:50 +0530

    feat: player two should play alternatively as O

--- src/components/TicTacToe.test.js
+++ src/components/TicTacToe.test.js
@@ -36,4 +36,19 @@ describe('TicTacToe component', () => {
     })
   });
 
+  test('Should show O when player two plays on a square alternatively', () => {
+    fireEvent.click(squares[Positions.TOP_LEFT_SQUARE]);
+    fireEvent.click(squares[Positions.CENTER_SQUARE]);
+
+    squares.forEach((square, position) => {
+      if (position === Positions.TOP_LEFT_SQUARE) {
+        expect(square.textContent).toBe(Constants.PLAYER_ONE_SYMBOL);
+      } else if (position === Positions.CENTER_SQUARE) {
+        expect(square.textContent).toBe(Constants.PLAYER_TWO_SYMBOL);
+      } else {
+        expect(square.textContent).toBe('');
+      }
+    })
+  });
+
 });
