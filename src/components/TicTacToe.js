faebb69fd3bdb42e6ebbcbe68ebd8d654ad46478
2022-DEV3-46 <2022dev3046@gmail.com>
2022-07-09 18:21:05 +0530

    feat: display game board with 3x3 empty squares

--- src/components/TicTacToe.js
+++ src/components/TicTacToe.js
@@ -2,11 +2,27 @@ import '../app.css';
 import { Constants } from '../constants/Constants';
 
 function TicTacToe() {
+
+  const renderBoard = () => {
+    return (
+      <div className='board'>
+        {
+          [...Array(Constants.TOTAL_SQUARES)].map((value, position) => {
+            return (
+              <button key={position} data-testid='square' className='square' />
+            )
+          }
+          )
+        }
+      </div>)
+  }
+
   return (
     <div className='App'>
       <header data-testid='header' className='App-header'>
         {Constants.HEADER}
       </header>
+      {renderBoard()}
     </div>
   );
 }
